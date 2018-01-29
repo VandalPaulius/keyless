8fc94a0523b558c5d89f5df360bd2a5a8d497ccf
Vandal Paulius Juska <aki.maxis@gmail.com>
2018-01-29 22:23:41 +0000

    Refactored reciever locking, unlocking, added custom sleeping times tailored for every event;

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -5,6 +5,8 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
+#include "lowPower.h"
+#include "Time.h"
 
 #define DEBUG
 
@@ -20,15 +22,22 @@
 
 #define PAYLOAD_SIZE        32
 #define SIGNAL_LOST_RETRIES 10
-#define SLEEP_TIME_MS       500
+//#define CHECK_BEACON_LOCKED 500
+#define CHECK_BEACON_LOCKED SLEEP_500MS
+#define CHECK_BEACON_UNLOCKED SLEEP_250MS
+
+#define CHECK_BEACON_UNLOCKED_DISCONNECTED SLEEP_60MS
+#define DISCONNECTED_BEACON_LOCK_THRESHOLD 60
 
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 char secret[32] = "77da4ba6-fdf2-11e7-8be5-0ed5ffff";
 
-uint8_t locked = true;
-uint8_t signalLost = 0;
+bool locked = true;
+bool preparingToLock = false;
+uint8_t signalLostCounter = 0; // TODO check for overflow values
 
 RF24 radio(CE, CSN);
+Time time;
 
 void setup(void) {
     #ifdef DEBUG
@@ -43,50 +52,80 @@ void setup(void) {
 }
 
 void loop(void) {
-    if (radio.available()) {
-        #ifdef DEBUG
-            printf("Radio available\r\n");
-        #endif
-        char message[PAYLOAD_SIZE + 1];
-        message[PAYLOAD_SIZE] = '\0';
+    //time.Timeout_manager();
+    // toggleLocks();
 
-        radio.read(message, PAYLOAD_SIZE);
+    if (locked) {
+        // time.Timer(
+        //     []() {
+        //         checkBeacon(signalLostCounter);
+        //     },
+        //     0,
+        //     SLEEP_TIME_LOCKED_MS,
+        //     1000
+        // );
+        checkBeacon(signalLostCounter, locked, secret);
 
-        #ifdef DEBUG
-            printf("%s\r\n", message);
-        #endif
-
-        if (strncmp(secret, message, sizeof(secret)) == 0) { // Secret matches
-
-            #ifdef DEBUG
-                printf("Secret matches\r\n");
-            #endif
-
-            signalLost = 0;
-            locked = false;
-        }
-    } else {
-        #ifdef DEBUG
-            printf("Signal lost. Retries: %i . Is locked: %i\r\n",
-                signalLost,
-                locked
-            );
-        #endif  
-        
-        if (signalLost >= SIGNAL_LOST_RETRIES){
-            locked = true;
+        if (locked) {
+            powerDown(CHECK_BEACON_LOCKED);
+            loop();
         } else {
-            signalLost++;
+
         }
+        // printf("Signal lost. Retries: %i . Is locked: %i\r\n",
+        //     signalLostCounter,
+        //     locked
+        // );
+        
     }
+    
+    if (!locked) {
+        bool connected = checkBeacon(signalLostCounter, locked, secret);
 
-    if (locked){
-        digitalWrite(SIGNAL_LOSS_INDICATOR, HIGH);
-    } else if (!locked){
-        digitalWrite(SIGNAL_LOSS_INDICATOR, LOW);
+        // if (hasConnection) {
+            
+        // } else {
+        //     // blink
+        // }
+
+        if (!connected) {
+            // blink
+            if (signalLostCounter >= DISCONNECTED_BEACON_LOCK_THRESHOLD) {
+                locked = true;
+                powerDown(CHECK_BEACON_LOCKED);
+            } else {
+                powerDown(CHECK_BEACON_UNLOCKED_DISCONNECTED);
+            }
+            signalLostCounter++;
+            loop();
+        }
+
+        powerDown(CHECK_BEACON_UNLOCKED);
+        loop();
     }
+    
+    // else {
+    //     #ifdef DEBUG
+    //         printf("Signal lost. Retries: %i . Is locked: %i\r\n",
+    //             signalLost,
+    //             locked
+    //         );
+    //     #endif  
+        
+    //     if (signalLost >= SIGNAL_LOST_RETRIES){
+    //         locked = true;
+    //     } else {
+    //         signalLost++;
+    //     }
+    // }
 
-    delay(SLEEP_TIME_MS);
+    // if (locked){
+    //     digitalWrite(SIGNAL_LOSS_INDICATOR, HIGH);
+    // } else if (!locked){
+    //     digitalWrite(SIGNAL_LOSS_INDICATOR, LOW);
+    // }
+
+    // delay(SLEEP_TIME_MS);
 }
 
 void initializePins() {
@@ -124,4 +163,60 @@ void systemInit() {
     PORTB = 0xff;
     PORTC = 0xff;
     PORTD = 0xff;
+}
+
+void indicateSignalLoss() {
+
+}
+
+// void TimerDrivercheckRadio() {
+//      // else {
+//     //     #ifdef DEBUG
+//     //         printf("Signal lost. Retries: %i . Is locked: %i\r\n",
+//     //             signalLost,
+//     //             locked
+//     //         );
+//     //     #endif  
+        
+//     //     if (!locked) {
+//     //         preparingToLock = true;
+//     //     } else {
+//     //         locked = true;
+//     //         preparingToLock = false;   
+//     //     }
+//     // }
+// }
+
+bool checkBeacon(uint8_t &signalLostCounter, bool &locked, char secret[]) {
+    if (radio.available()) {
+        #ifdef DEBUG
+            printf("Radio available\r\n");
+        #endif
+        char message[PAYLOAD_SIZE + 1];
+        message[PAYLOAD_SIZE] = '\0';
+
+        radio.read(message, PAYLOAD_SIZE);
+
+        #ifdef DEBUG
+            printf("Secret: %s Message: %s\r\n", secret, message);
+        #endif
+
+        if (strncmp(secret, message, sizeof(secret)) == 0) { // Secret matches
+
+            #ifdef DEBUG
+                printf("Secret matches\r\n");
+            #endif
+
+            signalLostCounter = 0;
+            locked = false;
+            return true;
+        }
+        return false;
+    } else {
+        return false;
+    }
+}
+
+void powerDown(period_t period) {
+    LowPower.powerDown(period, ADC_OFF, BOD_OFF);
 }
