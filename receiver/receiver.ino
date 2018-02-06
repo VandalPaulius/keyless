af681af7d7d2c61a644404caf8313ed9606d6b57
Danius KAlvaitis <danius.kalvaitis@gmail.com>
2018-02-06 15:38:08 +0000

    Implemented interrupt flags

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -8,7 +8,7 @@
 #include "printf.h"
 #include "lowPower.h"
 
-// #define DEBUG
+//#define DEBUG
 
 // User Interface
 #define BEACON_NEARBY_INDICATOR 5
@@ -25,10 +25,14 @@
 
 #define PAYLOAD_SIZE        32
 
-#define LOCKED_POWERDOWN_PERIOD         SLEEP_FOREVER
-#define UNLOCKED_POWERDOWN_PERIOD       SLEEP_2S
-#define IGNITION_ON_POWERDOWN_PERIOD    SLEEP_2S
-#define SIGNAL_LOST_POWERDOWN_PERIOD    SLEEP_8S
+#define LOCKED_POWERDOWN_PERIOD                     SLEEP_FOREVER
+#define UNLOCKED_POWERDOWN_PERIOD                   SLEEP_2S
+#define UNLOCKED_SIGNAL_LOST_POWERDOWN_PERIOD       SLEEP_120MS
+#define IGNITION_ON_POWERDOWN_PERIOD                SLEEP_2S
+#define IGNITION_ON_SIGNAL_LOST_POWERDOWN_PERIOD    SLEEP_120MS
+
+#define UNLOCKED_SIGNAL_LOST_LIMIT                  20
+#define IGNITION_ON_SIGNAL_LOST_LIMIT               100             
 
 const uint64_t pipe = 0xE8E8F0F0E1LL;
 char secret[32] = "77da4ba6-fdf2-11e7-8be5-0ed5ffff"; // line ending char
@@ -39,19 +43,14 @@ RF24 radio(CE, CSN);
 enum state_t{
     LOCKED,
     UNLOCKED,
+    UNLOCKED_SIGNAL_LOST,
     IGNITION_ON,
-    SIGNAL_LOST
+    IGNITION_ON_SIGNAL_LOST
 };
 
-enum interrupt_causes_t{
-    NRF24_INTERRUPT,
-    POWER_BUTTON_INTERRUPT,
-    TIMEOUT_INTERRUPT,
-    UNDEFINED
-};
-
-uint8_t program_state = LOCKED, next_state = LOCKED, powerdown_period = LOCKED_POWERDOWN_PERIOD;
-volatile uint8_t interrupt_cause = TIMEOUT_INTERRUPT;
+uint8_t program_state = LOCKED, next_state = LOCKED, powerdown_period = LOCKED_POWERDOWN_PERIOD, signal_loss_counter = 0;
+volatile uint8_t nrf24_interrupt_flag = false, power_button_interrupt_flag = false, timeout_interrupt_flag = false;
+volatile uint8_t portd_history = 0xFF; 
 
 void setup(void) {
     #ifdef DEBUG
@@ -67,9 +66,14 @@ void setup(void) {
 }
 
 void loop(void) {
+    #ifdef DEBUG
+        debugStatePrintout();
+    #endif
+
     switch(program_state){
-        case LOCKED:
-            if(interrupt_cause == NRF24_INTERRUPT){
+        case LOCKED:          
+            if(nrf24_interrupt_flag){
+                nrf24_interrupt_flag = false;
                 if(checkSecret()){
                     clearTimeout();
                     next_state = UNLOCKED;
@@ -77,43 +81,79 @@ void loop(void) {
             }
             break;
         case UNLOCKED:
-            if(interrupt_cause == NRF24_INTERRUPT){
+            if(nrf24_interrupt_flag){
+                nrf24_interrupt_flag = false;
                 if(checkSecret()){
                     clearTimeout();
                 }
             }
-            else if(interrupt_cause == POWER_BUTTON_INTERRUPT){
+            if(power_button_interrupt_flag){
+                power_button_interrupt_flag = false;
                 next_state = IGNITION_ON;
             }
-            else if(interrupt_cause == TIMEOUT_INTERRUPT){
-                next_state = LOCKED;
+            if(timeout_interrupt_flag){
+                timeout_interrupt_flag = false;
+                next_state = UNLOCKED_SIGNAL_LOST;
+            }
+            break;
+        case UNLOCKED_SIGNAL_LOST:
+            if(nrf24_interrupt_flag){
+                nrf24_interrupt_flag = false;
+                if(checkSecret()){
+                    clearTimeout();
+                    next_state = UNLOCKED;
+                }
+            }
+            if(timeout_interrupt_flag){
+                timeout_interrupt_flag = false;
+                if(signal_loss_counter >= UNLOCKED_SIGNAL_LOST_LIMIT){
+                    signal_loss_counter = 0;
+                    next_state = LOCKED;
+                }
+                else{
+                    digitalWrite(BEACON_NEARBY_INDICATOR, !digitalRead(BEACON_NEARBY_INDICATOR));
+                    signal_loss_counter++;
+                }  
             }
             break;
         case IGNITION_ON:
-            if(interrupt_cause == NRF24_INTERRUPT){
+            if(nrf24_interrupt_flag){
+                nrf24_interrupt_flag = false;
                 if(checkSecret()){
                     clearTimeout();
                 }
             }
-            else if(interrupt_cause == POWER_BUTTON_INTERRUPT){
+            if(power_button_interrupt_flag){
+                power_button_interrupt_flag = false;
                 next_state = UNLOCKED;
             }
-            else if(interrupt_cause == TIMEOUT_INTERRUPT){
-                next_state = SIGNAL_LOST;
+            if(timeout_interrupt_flag){
+                timeout_interrupt_flag = false;
+                next_state = IGNITION_ON_SIGNAL_LOST;
             }
             break;
-        case SIGNAL_LOST:
-            if(interrupt_cause == NRF24_INTERRUPT){
+        case IGNITION_ON_SIGNAL_LOST:
+            if(nrf24_interrupt_flag){
+                nrf24_interrupt_flag = false;
                 if(checkSecret()){
                     clearTimeout();
                     next_state = IGNITION_ON;
                 }
             }
-            else if(interrupt_cause == POWER_BUTTON_INTERRUPT){
+            if(power_button_interrupt_flag){
+                power_button_interrupt_flag = false;
                 next_state = LOCKED;
             }
-            else if(interrupt_cause == TIMEOUT_INTERRUPT){
-                next_state = LOCKED;
+            if(timeout_interrupt_flag){
+                timeout_interrupt_flag = false;
+                if(signal_loss_counter >= IGNITION_ON_SIGNAL_LOST_LIMIT){
+                    signal_loss_counter = 0;
+                    next_state = LOCKED;
+                }
+                else{
+                    digitalWrite(SIGNAL_LOSS_INDICATOR, !digitalRead(SIGNAL_LOSS_INDICATOR));
+                    signal_loss_counter++;
+                }
             }
             break;
     }
@@ -133,22 +173,28 @@ void loop(void) {
                 powerdown_period = UNLOCKED_POWERDOWN_PERIOD;
                 program_state = UNLOCKED;
                 break;
+            case UNLOCKED_SIGNAL_LOST:
+                digitalWrite(BEACON_NEARBY_INDICATOR, HIGH);
+                powerdown_period = UNLOCKED_SIGNAL_LOST_POWERDOWN_PERIOD;
+                program_state = UNLOCKED_SIGNAL_LOST;
+                break;
             case IGNITION_ON:
                 digitalWrite(SIGNAL_LOSS_INDICATOR, HIGH); // Turn off signal lost indicator
                 digitalWrite(RELAY, LOW); // Turn on relay
                 powerdown_period = IGNITION_ON_POWERDOWN_PERIOD;
                 program_state = IGNITION_ON;
                 break;
-            case SIGNAL_LOST:
+            case IGNITION_ON_SIGNAL_LOST:
                 digitalWrite(SIGNAL_LOSS_INDICATOR, LOW); // Turn on signal lost indicator
-                powerdown_period = SIGNAL_LOST_POWERDOWN_PERIOD;
-                program_state = SIGNAL_LOST;
+                powerdown_period = IGNITION_ON_SIGNAL_LOST_POWERDOWN_PERIOD;
+                program_state = IGNITION_ON_SIGNAL_LOST;
                 break;
         }
     }
     
-    interrupt_cause = UNDEFINED;
-    LowPower.powerDown(powerdown_period, BOD_OFF);
+    if(!nrf24_interrupt_flag && !power_button_interrupt_flag && !timeout_interrupt_flag){
+        LowPower.powerDown(powerdown_period, BOD_OFF);
+    }
 }
 
 void systemInit() {    
@@ -178,6 +224,7 @@ void initializePins() {
     pinMode(BEACON_NEARBY_INDICATOR, OUTPUT);
     pinMode(POWER_TOGGLE_BUTTON, INPUT);
     pinMode(RELAY, OUTPUT);
+    pinMode(NRF_IRQ, INPUT);
 }
 
 void initializeRadio() {
@@ -202,16 +249,31 @@ void pciSetup(byte pin){
 
 ISR(WDT_vect) {
     wdt_disable();
-    interrupt_cause = TIMEOUT_INTERRUPT;
+    timeout_interrupt_flag = true;
 }
 
 ISR(PCINT2_vect) { // handle pin change interrupt
-    if(!digitalRead(NRF_IRQ)){
-        interrupt_cause = NRF24_INTERRUPT;    
+    uint8_t changed_pins;
+
+    changed_pins = PIND ^ portd_history;
+    portd_history = PIND;
+
+    if((changed_pins & (1 << 4)) && (~portd_history & (1 << 4))){
+        nrf24_interrupt_flag = true;   
     }
-    else if (!digitalRead(POWER_TOGGLE_BUTTON)) {
-        interrupt_cause = POWER_BUTTON_INTERRUPT;
+
+    if((changed_pins & (1 << 7)) && (~portd_history & (1 << 7))){
+        power_button_interrupt_flag = true;   
     }
+
+    
+    /*if(!digitalRead(NRF_IRQ)){
+        nrf24_interrupt_flag = true;   
+    }
+    
+    if (digitalRead(POWER_TOGGLE_BUTTON)) {
+        power_button_interrupt_flag = true;
+    }*/
 }
 
 void clearTimeout(){
@@ -229,7 +291,7 @@ bool checkSecret(){
         radio.read(message, PAYLOAD_SIZE);
 
         #ifdef DEBUG
-            printf("Secret: %s Message: %s\r\n", secret, message);
+            printf("Message: %s\r\n", message);
         #endif
 
         if (strncmp(secret, message, sizeof(secret)) == 0) { // Secret matches
@@ -243,3 +305,40 @@ bool checkSecret(){
     }
     return false;
 }
+
+#ifdef DEBUG
+    void debugStatePrintout(){
+        switch(program_state){
+            case LOCKED:
+                printf("LOCKED\t");
+                break;
+            case UNLOCKED:
+                printf("UNLOCKED\t");
+                break;
+            case IGNITION_ON:
+                printf("IGNITION_ON\t");
+                break;
+            case IGNITION_ON_SIGNAL_LOST:
+                printf("IGNITION_ON_SIGNAL_LOST\t");
+                break;
+            default:
+                printf("UNDEFINED\t");
+        }
+
+        switch(interrupt_cause){
+            case NRF24_INTERRUPT:
+                printf("NRF24_INTERRUPT\r\n");
+                break;
+            case POWER_BUTTON_INTERRUPT:
+                printf("POWER_BUTTON_INTERRUPT\r\n");
+                break;
+            case TIMEOUT_INTERRUPT:
+                printf("TIMEOUT_INTERRUPT\r\n");
+                break;
+            case UNDEFINED:
+                printf("UNDEFINED\r\n");
+                break;
+        } 
+        delay(10);     
+    }
+#endif
